4b319ca27397e2a7ab80f1164c95423cf547995a
Ryan Groenewold <ryangroenewold@hotmail.com>
2017-12-21 14:33:12 +0100

    UPstream changes

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+<<<<<<< Updated upstream
 # import all libraries and load the menu.
+=======
+>>>>>>> Stashed changes
 import pygame
 import sys
 import os
 import menu
 import astrodoge
+<<<<<<< Updated upstream
 import splaceholder
  
 menu.start_menu()
 
  
+=======
+from time import sleep
+
+
+menu.start_menu()
+# astrodoge.start_astrodoge()
+
+>>>>>>> Stashed changes
