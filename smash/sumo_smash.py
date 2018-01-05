012d7c9ea1703bcf47899c9cf8e4d4fbcce8424d
kimber9812 <33624149+kimber9812@users.noreply.github.com>
2018-01-05 16:46:03 +0100

    update
    hit wals you die
    walls com closer

--- smash/sumo_smash.py
+++ smash/sumo_smash.py
@@ -30,38 +30,44 @@ class Player(pygame.sprite.Sprite):
         self.rect.centery = HEIGHT / 2
         self.speedx = 0
         self.speedy = 0
+        self.alive = True
 
     def update(self):
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
-                self.speedx = -10
+                self.speedx = -12
                 self.speedy = 0
         if keystate[pygame.K_RIGHT]:
-                self.speedx = 10
+                self.speedx = 12
                 self.speedy = 0
         if keystate[pygame.K_UP]:
-                self.speedy = -10
+                self.speedy = -12
                 self.speedx = 0
         if keystate[pygame.K_DOWN]:
-                self.speedy = 10  
+                self.speedy = 12 
                 self.speedx = 0     
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.right > 850:
-           self.rect.right = 850 
+           self.rect.right = 860
+           self.alive = False
         if self.rect.left < 50:
            self.rect.left = 50
+           self.alive = False
         if self.rect.top < 50:
            self.rect.top = 50
+           self.alive = False
         if self.rect.bottom > 850:
-           self.rect.bottom = 850   
+           self.rect.bottom = 850 
+           self.alive = False
+
 
 
 class Mob1(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((60, 60))
-        self.image.fill(YELLOW)
+        self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(-500, -400)
         self.rect.y = random.randrange(HEIGHT - self.rect.height)
@@ -95,42 +101,42 @@ class Mob3(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((60, 60))
-        self.image.fill(BLUE)
+        self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.x = 950
-        self.rect.y = 300
-        self.speedy = 15
+        self.rect.x = 1200
+        self.rect.y = random.randrange(50, 850)
+        self.speedy = random.randrange(10, 15)
 
     def update(self):
         self.rect.x -= self.speedy
         if self.rect.left < -50:
             self.rect.x = 950
-            self.rect.y = 300
-            self.speedx = 15
+            self.rect.y = random.randrange(50, 850)
+            self.speedx = random.randrange(10, 15)
 
 class Mob4(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((60, 60))
-        self.image.fill(BLACK)
+        self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(1, 850)
-        self.rect.y = random.randrange(900, 1000)
+        self.rect.x = random.randrange(50, 850)
+        self.rect.y = 1200
         self.speedy = random.randrange(10, 15)
 
     def update(self):
         self.rect.y -= self.speedy
-    if self.rect.top < 50:
-        self.rect.x = random.randrange(1, 850)
-        self.rect.y = random.randrange(900, 1000)
-        self.speedy = random.randrange(10, 15)
+        if self.rect.top < -50:
+            self.rect.x = random.randrange(50, 850)
+            self.rect.y = 1000
+            self.speedy = random.randrange(10, 15)
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 player = Player()
 walls = pygame.sprite.Group()
 all_sprites.add(player)
-for i in range(1):
+for i in range(2):
     m1 = Mob1()
     m2 = Mob2()
     m3 = Mob3()
@@ -138,10 +144,27 @@ for i in range(1):
     all_sprites.add(m1, m2, m3, m4)
     mobs.add(m1, m2, m3, m4)
 
+time = 10_000
+
+block_1 = -450
+block_2 = 850
+block_3 = 850
+block_4 = -450
 
 # Game loop
 running = True
 while running:
+    time = time - 1
+    if time == 9750: 
+        block_1 = -400
+        block_2 = 800
+        block_3 = 800
+        block_4 = -400
+    if time == 9500: 
+        block_1 = -350
+        block_2 = 750
+        block_3 = 750
+        block_4 = -350
     # keep loop running at the right speed
     clock.tick(FPS)
     # Process input (events)
@@ -157,15 +180,16 @@ while running:
     hits = pygame.sprite.spritecollide(player, mobs, False)
     if hits:
         running = False
-
+    if player.alive == False:
+        running = False
 
 
     # Draw / render
     screen.fill(WHITE)
-    pygame.draw.rect(screen, RED, [-450, 0, 500, 900]) # left
-    pygame.draw.rect(screen, RED, [850, 0, 500, 900]) # Richt
-    pygame.draw.rect(screen, RED, [0, 850, 900, 500]) # bottom
-    pygame.draw.rect(screen, RED, [0, -450, 900, 500]) # top
+    pygame.draw.rect(screen, RED, [block_1, 0, 500, 900]) # left
+    pygame.draw.rect(screen, RED, [block_2, 0, 500, 900]) # Richt
+    pygame.draw.rect(screen, RED, [0, block_3, 900, 500]) # bottom
+    pygame.draw.rect(screen, RED, [0, block_4, 900, 500]) # top
     all_sprites.draw(screen)
     # *after* drawing everything, flip the display
     pygame.display.flip()
