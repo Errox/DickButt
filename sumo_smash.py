61ecb852494105ded5d948a43d34a7e9bb20f9b2
kimber9812 <33624149+kimber9812@users.noreply.github.com>
2018-01-08 18:21:45 +0100

    visual score
    de score staat er nu bij

--- sumo_smash.py
+++ sumo_smash.py
@@ -16,6 +16,11 @@ def start_sumo_smash():
     BLUE = (0, 0, 255)
     YELLOW = (255, 255, 0)
 
+    time = 10_000
+    count = 0 
+
+    font = pygame.font.SysFont('Arcadepix.ttf', 30) 
+
     # initialize pygame and create window
     pygame.init()
     pygame.mixer.init()
@@ -70,7 +75,7 @@ def start_sumo_smash():
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.Surface((60, 60))
-            self.image.fill(RED)
+            self.image.fill(BLUE)
             self.rect = self.image.get_rect()
             self.rect.x = random.randrange(-500, -400)
             self.rect.y = random.randrange(HEIGHT - self.rect.height)
@@ -87,7 +92,7 @@ def start_sumo_smash():
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.Surface((60, 60))
-            self.image.fill(RED)
+            self.image.fill(BLUE)
             self.rect = self.image.get_rect()
             self.rect.x = random.randrange(WIDTH - self.rect.width)
             self.rect.y = random.randrange(-500, -400)
@@ -104,7 +109,7 @@ def start_sumo_smash():
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.Surface((60, 60))
-            self.image.fill(RED)
+            self.image.fill(BLUE)
             self.rect = self.image.get_rect()
             self.rect.x = 1200
             self.rect.y = random.randrange(50, 850)
@@ -121,7 +126,7 @@ def start_sumo_smash():
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.image = pygame.Surface((60, 60))
-            self.image.fill(RED)
+            self.image.fill(BLUE)
             self.rect = self.image.get_rect()
             self.rect.x = random.randrange(50, 850)
             self.rect.y = 1200
@@ -139,36 +144,73 @@ def start_sumo_smash():
     player = Player()
     walls = pygame.sprite.Group()
     all_sprites.add(player)
-    for i in range(2):
+    for i in range(1):
         m1 = Mob1()
         m2 = Mob2()
         m3 = Mob3()
         m4 = Mob4()
         all_sprites.add(m1, m2, m3, m4)
         mobs.add(m1, m2, m3, m4)
+    if time == 9800:
+        for i in range(2):
+            m1 = Mob1()
+            m2 = Mob2()
+            m3 = Mob3()
+            m4 = Mob4()
+            all_sprites.add(m1, m2, m3, m4)
+            mobs.add(m1, m2, m3, m4)
 
-    time = 10_000
-    count = 0
 
     block_1 = -450
     block_2 = 850
     block_3 = 850
     block_4 = -450
 
+    backround = YELLOW
+
     # Game loop
     running = True
     while running:
         time = time - 1
         if time == 9750: 
-            block_1 = -450
-            block_2 = 850
-            block_3 = 850
-            block_4 = -450
+            block_1 = -400
+            block_2 = 800
+            block_3 = 800
+            block_4 = -400
         if time == 9500: 
             block_1 = -350
             block_2 = 750
             block_3 = 750
             block_4 = -350
+        if time == 9750:
+            for i in range(1):
+                m1 = Mob1()
+                m2 = Mob2()
+                m3 = Mob3()
+                m4 = Mob4()
+                all_sprites.add(m1, m2, m3, m4)
+                mobs.add(m1, m2, m3, m4)
+        if time == 9500:
+            for i in range(1):
+                m1 = Mob1()
+                m2 = Mob2()
+                m3 = Mob3()
+                m4 = Mob4()
+                all_sprites.add(m1, m2, m3, m4)
+                mobs.add(m1, m2, m3, m4)
+        if time == 9800:
+            backround = RED   
+        if time == 9790:
+            backround = YELLOW    
+        if time == 9780:
+            backround = RED   
+        if time == 9770:
+            backround = YELLOW 
+        if time == 9760:
+            backround = RED   
+        if time == 9750:
+            backround = YELLOW 
+        print (time)
         # keep loop running at the right speed
         clock.tick(FPS)
         # Process input (events)
@@ -177,8 +219,8 @@ def start_sumo_smash():
             if event.type == pygame.QUIT:
                 running = False
         
-        count += 1
-        score = int(count / 3)
+        count += 100
+        score = int(count)
 
         # Update
         all_sprites.update()
@@ -191,13 +233,16 @@ def start_sumo_smash():
             running = False
             game_over.start(score) 
 
-        # Draw / render
-        screen.fill(WHITE)
-        pygame.draw.rect(screen, RED, [block_1, 0, 500, 900]) # left
-        pygame.draw.rect(screen, RED, [block_2, 0, 500, 900]) # Richt
-        pygame.draw.rect(screen, RED, [0, block_3, 900, 500]) # bottom
-        pygame.draw.rect(screen, RED, [0, block_4, 900, 500]) # top
+
+       
+        screen.fill(backround)
+        pygame.draw.rect(screen, BLACK, [block_1, 0, 500, 900]) # left
+        pygame.draw.rect(screen, BLACK, [block_2, 0, 500, 900]) # Richt
+        pygame.draw.rect(screen, BLACK, [0, block_3, 900, 500]) # bottom
+        pygame.draw.rect(screen, BLACK, [0, block_4, 900, 500]) # top
         all_sprites.draw(screen)
+        scoretext = font.render("Score {0}".format(score), 1, WHITE)
+        screen.blit(scoretext, (5, 10))
         # *after* drawing everything, flip the display
         pygame.display.flip()
 
